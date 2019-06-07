95a9a2bf8f19758c7369018289f05ba0547f2bf3
Joe Pell <jpell3130@gmail.com>
2019-06-06 23:26:08 -0400

    bubble sort and substrings

--- phase_2_enumerable.js
+++ phase_2_enumerable.js
@@ -23,4 +23,33 @@ Array.prototype.myReduce = function (callback) {
   return accumulator;
 }
 
+Array.prototype.bubbleSort = function () {
+  var sorted = true
+  for (let i = 1; i < this.length; i++) {
+    var currentNum = this[i]
+    var prevNum = this[i-1]
+    if (prevNum > currentNum) {
+      this[i] = prevNum
+      this[i-1] = currentNum
+      sorted = false
+    }
+  }
 
+  if (!sorted) {
+    return this.bubbleSort();
+  } else {
+    return this;
+  }
+}
+
+var substrings = function (string) {
+  var subs = [];
+
+  string.split("").forEach(function (letter, i) {
+    for (j = i + 1; j <= string.length; j++) {
+      subs.push(string.slice(i, j));
+    }
+  });
+
+  return subs;
+};
